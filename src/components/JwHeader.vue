8fa51d6629a24bd92ff67b162cc91de526a50398
DESKTOP-LK5JBDR\BaoYikun <1363285993@qq.com>
2021-01-21 11:53:30 +0800

    hh

--- src/components/JwHeader.vue
+++ src/components/JwHeader.vue
@@ -28,6 +28,39 @@
         <img src="../assets/img/login-button-icon.png" alt=""/>
         <span>登陆</span>
       </div>
+      <!-- 登陆后 -->
+      <div class="login-after f-r vertical-center" v-if="userId!=''&&userId!=null&&userId!=undefined">
+        <!--        <img-->
+        <!--          class="portClass c-u f-l"-->
+        <!--          :src="-->
+        <!--            portType-->
+        <!--              ? require('../assets/img/student.png')-->
+        <!--              : require('../assets/img/teacher.png')-->
+        <!--          "-->
+        <!--          alt=""-->
+        <!--          @click="switchPort"-->
+        <!--        />-->
+        <img
+          class="portClass c-u f-l"
+          :src="
+            portType=require('../assets/img/teacher.png')
+          "
+          alt=""
+          @click="switchPort"
+        />
+        <div class="info-mess f-l p-r c-u" @mousemove="showHlep = true" @mouseleave="showHlep = false">
+          <div class="tximg-box p-r">
+            <img class="tx-img" :src="avatarUrl" alt="">
+            <span class="mess-num p-a">20</span>
+          </div>
+          <span class="login-name">{{ roleName }}</span>
+          <img class="down" src="../assets/img/down.png" alt="">
+        </div>
+        <ul class="pull-down p-a" v-if="showHlep" @mousemove="showHlep = true" @mouseleave="showHlep = false">
+          <li class="c-u">帮助中心</li>
+          <li class="c-u">退出</li>
+        </ul>
+      </div>
       <!-- 站位 -->
       <div class="suspend">
         <ul>
@@ -65,9 +98,11 @@ export default {
     return {
       headeList: [],
       activeStatus: 0,
-      avatarUrl:"",
-      roleName:"",
-      userId:""
+      avatarUrl: "",
+      roleName: "",
+      userId: "",
+      portType: false,
+      showHlep: false
     };
   },
   created() {
@@ -77,6 +112,11 @@ export default {
     this.activeStatus = url;
   },
   methods: {
+    // 切换端
+    switchPort() {
+      this.portType = !this.portType;
+      window.location.href = "http://www.estudy.nsa/study/class";
+    },
     checkMenu(item) {
       this.activeStatus = item.id;
       this.$router.push({path: item.path, query: {id: item.id}})
@@ -103,13 +143,16 @@ export default {
       document.body.scrollTop = document.documentElement.scrollTop = 0;
     },
     login() {
-      // window.location.href = "http://www.estudy.nsa/sso/login?tenant_id=1&service=http%3A%2F%2Fwww.estudy.nsa%2F";
-      window.location.href = "http://www.estudy.nsa/study/class";
+      window.location.href = "http://www.estudy.nsa/sso/login?tenant_id=1&service=http%3A%2F%2Fwww.estudy.nsa%2F";
     },
-    getLoginInfo(){
-      var userId=this.$common.getUrlKey("ticket",window.location.href);
-      this.$get("cms-web/user/api",{ticket:userId}).then(res=>{
-
+    getLoginInfo() {
+      var userId = this.$common.getUrlKey("ticket", window.location.href);
+      this.$get("cms-web/user/api", {ticket: userId}).then(res => {
+        if (res.data != null && res.data != undefined) {
+          this.avatarUrl = res.data.avatarUrl;
+          this.roleName = res.data.name;
+          this.userId = res.data.userId;
+        }
       });
     }
   },
@@ -196,7 +239,7 @@ export default {
 
     li {
       margin-top: 20px;
-      background-color: rgba(127, 178, 221, .9);
+      background-color: rgba(127, 178, 221, 0.9);
       width: 60px;
       height: 60px;
       text-align: center;
@@ -340,6 +383,90 @@ export default {
       .h-menu-list:hover,
       .activeClass {
         background-color: #4c89dc;
+      }
+    }
+
+    .login-after {
+      right: 128px;
+      width: 290px;
+      height: 38px;
+      margin-top: -19px;
+
+      .info-mess {
+        line-height: 38px;
+      }
+
+      .portClass {
+        width: 125px;
+        height: 38px;
+        margin-right: 20px;
+      }
+
+      .tximg-box {
+        height: 35px;
+        width: 35px;
+        border-radius: 50%;
+        float: left;
+        margin-right: 12px;
+        line-height: 35px;
+
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+          background-color: #f7f7f7;
+        }
+
+        .mess-num {
+          right: -10px;
+          top: -5px;
+          height: 18px;
+          line-height: 18px;
+          background: #ff5e4a;
+          color: #fff;
+          font-size: 12px;
+          border-radius: 13px;
+          padding: 0 5px;
+        }
+      }
+
+      .login-name {
+        line-height: normal;
+        font-size: 15px;
+        color: #fff;
+
+        &:hover {
+          color: #0067f7;
+        }
+      }
+
+      .down {
+        width: 15px;
+        height: 10px;
+        line-height: 15px;
+      }
+
+      .pull-down {
+        width: 110px;
+        background-color: #ffffff;
+        color: #333333;
+        right: 0;
+        top: 40px;
+        text-align: center;
+        border-radius: 6px;
+        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, .2);
+        // display: none;
+        // &:hover{
+        //   display: inline-block;
+        // }
+        li {
+          font-size: 16px;
+          line-height: 50px;
+
+          &:hover {
+            color: #0067f7;
+          }
+        }
       }
     }
 
